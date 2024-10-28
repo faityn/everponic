159a2e0af46e89d6bf136de647d259ac6573ea9e
Baaska <faitynb@gmail.com>
2024-10-28 20:06:42 +0800

    gg

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,17 +6,19 @@ import { appWithTranslation } from "next-i18next";
 import { useRouter } from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
 import { RecoilRoot } from "recoil";
+
 function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  return (
-    <RecoilRoot>
-      <AnimatePresence>
-        <motion.div key={router.route} className="h-full">
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </RecoilRoot>
-  );
+    const router = useRouter();
+
+    return (
+        <RecoilRoot>
+            <AnimatePresence>
+                <motion.div key={router.route} className="h-full">
+                    <Component {...pageProps} />
+                </motion.div>
+            </AnimatePresence>
+        </RecoilRoot>
+    );
 }
 
 export default appWithTranslation(App);
